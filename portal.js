1a565ac20fd63c6166a821a9eec3c65934991d97
rl3075 <rl3075@nyu.edu>
2017-04-27 11:42:15 -0400

    Add files via upload
    new revised code for Beta version

--- portal.js
+++ portal.js
@@ -1,10 +1,9 @@
 
 function Portal(color, xpos, ypos, width, height) { //creates the two portals, passing 
-var rad = 10;
-
-
+//var rad = 10;
+//var theta = 0;
 //scribble.bowing = 0;	
-// 						//passing variables into their class
+//passing variables into their class
 	this.display = function() {
 	strokeWeight(7);
 	stroke(color);
@@ -13,16 +12,30 @@ var rad = 10;
    	//image(img4, 975, 550, 100, 75); //for sale sign 
 	}
 
-	this.travel = function() {
-	 noStroke(); //this gives the eyes a color effect as well
-     var x = rad * cos(theta);
-  	 var y = rad * sin(theta);
- 	 ellipse(x + width / 2, y + height / 2, 30, 30);
-  	 theta = theta + 2;
-  	 rad = rad + .5;
-	}
+	this.travelf = function() {
+	background('#619BF2');
+	 for (r = 0; r < 1000; r = r + .5) { //I used a for loop to create a spiral
+     noStroke(); //at first I just a normal loop but it would create the spiral slowly
+     var x = r * cos(theta); //after experimenting I was able to exuecute a solid spinning spiral
+     var y = r * sin(theta);
+     fill('#C79BF2'); //I used the Learning Procc. book to figure this out 
+     ellipse(x + 700, y + 400, 25, 25);
+     theta = theta + 50;
+     }
+	} 
 
+    this.travelb = function() {
+	background('#C79BF2');
+	 for (r = 0; r < 1000; r = r + .5) { //I used a for loop to create a spiral
+     noStroke(); //at first I just a normal loop but it would create the spiral slowly
+     var x = r * cos(theta); //after experimenting I was able to exuecute a solid spinning spiral
+     var y = r * sin(theta);
+     fill('#619BF2'); //I used the Learning Procc. book to figure this out 
+     ellipse(x + 700, y + 400, 35, 35);
+     theta = theta + 1000;
 
+     }
+	} 
 
 
 }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
-var teleport = 5; 
-var key = false;
-var player_sprite_sheet;
+var teleport = 0; 
+var tp = 1;
+var player_sprite_sheet; //these are used to create the sprite
 var player_walk; 
 var player_stand;
 var player_sprite;
-var scribble = new Scribble();    
+var dancer; //dancing person
+var scribble = new Scribble(); //allows you to use scribble library  
 var portal1; //2 objects of the same class
 var portal2;
 var you;
 var mySound;
-var changes1;
+var familySound; //dialouge for family 
 var img; //variable for image of background
 var img2; //image for store
 var img3; //image for apartments
@@ -20,9 +21,19 @@ var img7; //starbucks store
 var img8; //construction sign
 var img9; //caution tape
 var img10; //new apt
-var img11;
+var img11; //construction site
+var img12; //news
+var img13; //newspaper
+var img14; //family
+var img15; //table
+var img16; //handshake 
 var introFont;
-var motion = 0;
+var motion = 0; //used with key to control sprite movement
+var ourtime; //courrent time
+var timeStamp; //time stamp
+var rad = 10; //these are used for the portal visuals
+var theta = 0; 
+
 
 var player_frames = [ //splites the png into frames for animation
   {"name":"player_walk01", "frame":{"x":0, "y": 0, "width": 100, "height": 400}},
@@ -35,6 +46,8 @@ var player_frames = [ //splites the png into frames for animation
   {"name":"player_walk08", "frame":{"x":2065, "y": 0, "width": 185, "height": 400}}
 ];
 
+
+
 function preload() {
  img = loadImage("assets/background.jpg");
  img2 = loadImage("assets/store.png");
@@ -45,161 +58,171 @@ function preload() {
  img7 = loadImage("assets/starstore.png");
  img8 = loadImage("assets/undercon.png");
  img9 = loadImage("assets/caution.png");
- img11 = loadImage("assets/constructionsite.png");
  img10 = loadImage("assets/newapt.png");
- introFont = loadFont("assets/GoodDog.otf");
+ img11 = loadImage("assets/constructionsite.png");
+ img12 = loadImage("assets/news.png");
+ img13 = loadImage("assets/newspaper.png");
+ img14 = loadImage("assets/family.png");
+ img15 = loadImage("assets/table.png");
+ img16 = loadImage("assets/handshake.png");
+
+ introFont = loadFont("assets/impact.ttf");
  mySound = loadSound('assets/song1.mp3');
-  // Load player sprite sheet from frames array
-  player_sprite_sheet = loadSpriteSheet('assets/sprite1.png', player_frames);
-  // Player walk animation passing in a SpriteSheet
-  player_walk = loadAnimation(player_sprite_sheet);
-  // An animation with a single frame for standing
-  player_stand = loadAnimation(new SpriteSheet('assets/sprite1.png',
+  familySound = loadSound('assets/family.m4a');
+  player_sprite_sheet = loadSpriteSheet('assets/sprite1.png', player_frames); // Load player sprite sheet from frames array
+  player_walk = loadAnimation(player_sprite_sheet); // Player walk animation passing in a SpriteSheet
+  player_stand = loadAnimation(new SpriteSheet('assets/sprite1.png',  // An animation with a single frame for standing
     [{"name":"player_stand", "frame":{"x":0, "y": 0, "width": 100, "height": 400}}]));
+  dancer = loadAnimation("assets/dancer_frames/0.png", "assets/dancer_frames/10.png");
 }
 
 function setup() {
-	mySound.setVolume(0.1);
-  mySound.play();
+	mySound.setVolume(0.5);
+  //mySound.play();
   createCanvas(1436, 815);
   frameRate(40);
    portal1 = new Portal('#619BF2', width - 40, height - 200, 70, 400); //spawn two portals on each side
    portal2 = new Portal('#C79BF2', 40, height - 200, 70, 400);
-   //changes1 = new Changes();
-    //firstB = new FirstB();
-    //you.setup();
-    //you.preload();
+   objects = new Objects(); //creates instance of Objects class
+
   // Create the Player sprite and add it's animations
   player_sprite = createSprite(width/2, 600, 100, 300);
   player_sprite.addAnimation('walk', player_walk);
   player_sprite.addAnimation('stand', player_stand);
-
 }
 
 function draw() {
   clear();
+  image(img, 0, 0, width, height-115); //spawn image of background
+	escenas(); //checks for teleportation to change scenes
+	objects.sidewalk(); //draws the sidewalk
+  objects.display(); //displays interavtive objects 
+  portal1.display(); //display the portals
+  portal2.display(); // one for each side 
+
+  if(keyDown(LEFT_ARROW)) { //thse keyboard commans check change motion variable and teleport you
+    motion = 1;
+    if(player_sprite.position.x === 88) {
+      player_sprite.position.x = 1383;
+      teleport = teleport - 1; 
+      tp = 1;
+      timeStamp = millis(); //stores current time
+      } 
+    }
+  else if(keyDown(RIGHT_ARROW)) {
+    motion = 2;
+    if(player_sprite.position.x === 1383) {
+      player_sprite.position.x = 88;
+      teleport = teleport + 1; 
+      tp = 0;
+      timeStamp = millis(); //stores current time
+      } 
+    }
+  else {
+        motion = 0;
+  }
+  
+  if(motion == 1) { //these if statements check the var motion to animate you
+      player_sprite.changeAnimation('walk');
+      player_sprite.mirrorX(-1); //flip horizontally
+      player_sprite.velocity.x = - 5; // move left
+  }
+  else if(motion == 2) {
+        player_sprite.changeAnimation('walk');
+        player_sprite.mirrorX(1); // flip horizontally
+        player_sprite.velocity.x = 5; // move right
+  }
+  else {
+      player_sprite.changeAnimation('stand'); //if no key is pressed, stand still
+      player_sprite.velocity.x = 0;
+  }
+
+    drawSprites(); //draw the sprites
+    
+    if(millis() - timeStamp < 1500) {
+      if(tp === 0) {
+       portal1.travelf();
+      }
+      if(tp === 1) {
+       portal1.travelb();
+      }
+    }
+
+     objects.interact(); //interact with the family 
+
+    mainmenu(); 
+} // draw
+
+function mainmenu() { //calls for main menu
+if (teleport === 0) {
   background(255);
-	image(img, 0, 0, width, height-115); //spawn image of background
-	if (teleport === 1) { //first scene
-	image(img2, width/2, 0, 700, 800); //store
-	image(img5, 652, 300, 85, 90); //boombox
-	image(img3, 100, -350, 700, 1100); //apt
-  textSize(25);
   fill(0);
-  text("COFFEE - $1", 900, 600);
-}
+  noStroke();
+  textSize(100);
+  textFont(introFont);
+  textAlign(CENTER);
+  text("GENTRIFIED", width/2, height/2-100);
+  textSize(20);
+  text("Press Enter to Play", width/2, height/2);
+  if (keyIsDown(ENTER))
+    teleport = teleport + 1;
+  } //if
+} //mainmenu
+
+function sidewalk() { //creates the sidewalk
+ 
+  } //sidewalk
+
+function escenas() {
+  if (teleport === 1) { //first scene
+    image(img2, width/2, 0, 700, 800); //store
+    image(img5, 652, 300, 85, 90); //boombox
+    image(img3, 100, -350, 700, 1100); //apt
+    textSize(25);
+    fill(0);
+    text("CAFÉ - $1", 900, 600);
+    animation(dancer, 535, 325);
+  }
 if (teleport === 2) { //second scene
-	image(img2, width/2, 0, 700, 800); //store
+  image(img2, width/2, 0, 700, 800); //store
   image(img5, 652, 300, 85, 90); //boombox
-	image(img3, 100, -350, 700, 1100); //apt
-  //changes1.display();
+  image(img3, 100, -350, 700, 1100); //apt
   image(img4, 975, 550, 100, 75); //for sale sign
-}
+  }
 if (teleport === 3) { //third scene
   image(img7, width/2, 0, 700, 800); //store
   image(img3, 100, -350, 700, 1100); //apt
-  //changes1.display();
   image(img6, 920, 290, 300, 125); //starbucks logo
   textSize(25);
   fill(0);
   text("COFFEE - $5", 900, 600);
-}
-if (teleport === 4) { //third scene
+ }
+if (teleport === 4) { //fourth scene
   image(img7, width/2, 0, 700, 800); //store
-  image(img3, 100, -350, 700, 1100); //apt
-  //changes1.display();
   image(img6, 920, 290, 300, 125); //starbucks logo
   textSize(25);
-  fill(0);
-  text("COFFEE - $5", 900, 600);
-}
-if (teleport === 5) { //third scene
-  image(img7, width/2, 0, 700, 800); //store
-  //image(img9, 100, 500, 300, 300); //apt
-  //changes1.display();
-  image(img6, 920, 290, 300, 125); //starbucks logo
-  textSize(25);
-  fill(0);
+  fill(0); 
   text("COFFEE - $5", 900, 600);
   image(img11, 150, 0, 600, 700);
   //image(img8, 250, 500, 300, 250); //construction sign
   image(img9, 85, 500, 700, 250); //caution tape
 }
-if (teleport === 6) { //third scene
+if (teleport === 5) { //fifth scene
   image(img7, width/2, 0, 700, 800); //store
   image(img10, 100, -100, 700, 900); //new apt
-  //changes1.display();
   image(img6, 920, 290, 300, 125); //starbucks logo
   textSize(25);
   fill(0);
   text("COFFEE - $5", 900, 600);
 }
+  } //escenas
 
-	rectMode(CENTER);
-	fill(100);
-	strokeWeight(10);
-	rect(width/2, height, width, 230);
-    scribble.bowing = 5;
-    scribble.maxOffset = 1;
-    strokeWeight(5);
-    scribble.scribbleFilling([0, width, width, 0], [700, 700, height, height], 100, 10);
-    strokeWeight(5);
-    //scribble.maxOffset = 0;
-    //scribble.scribbleRect(width/2, height, width, 230);
-    scribble.maxOffset = 0;	
-    portal1.display(); //display the portals
-    portal2.display();
-         
-     if(keyDown(LEFT_ARROW)) { //thse keyboard commans check change motion variable and teleport you
-  motion = 1;
-    if(player_sprite.position.x === 88) {
-      player_sprite.position.x = 1383;
-      teleport = teleport - 1; 
-    } 
-      }
-      else if(keyDown(RIGHT_ARROW)) {
-    motion = 2;
-    if(player_sprite.position.x === 1383) {
-      player_sprite.position.x = 88;
-      teleport = teleport + 1; 
-    } 
-        }
-      else {
-        motion = 0;
-      }
-  
-      if(motion == 1) { //these if statements check the var motion to animate you
-      player_sprite.changeAnimation('walk');
-    // flip horizontally
-      player_sprite.mirrorX(-1);
-    // move left
-      player_sprite.velocity.x = - 5; 
-      }
-      else if(motion == 2) {
-        player_sprite.changeAnimation('walk');
-    // flip horizontally
-        player_sprite.mirrorX(1);
-    // move right
-        player_sprite.velocity.x = 5;
-      }
-      else {
-      player_sprite.changeAnimation('stand');
-    //if close to the mouse, don't move
-      player_sprite.velocity.x = 0;
-      }
 
-    drawSprites(); //draw the sprites
- 
- if (teleport === 0) {
- 	background(255);
- 	fill(0);
- 	noStroke();
- 	textSize(100);
- 	textFont(introFont);
- 	text("GENTRIFIED", 550, height/2-100);
- 	textSize(20);
- 	text("Press Enter Bar to Play", 600, height/2);
- 	if (keyIsDown(ENTER))
-    teleport = teleport + 1;
- }
-} // draw
+  function mousePressed() {
+    print(mouseX)
+   if (mouseIsPressed && mouseX > 299 && mouseX < 473 && mouseY < 726 && mouseY > 384 && teleport === 3) {
+     mySound.playMode('restart');
+     familySound.setVolume(1);
+     familySound.play();
+    }
+  } //mousePressed
